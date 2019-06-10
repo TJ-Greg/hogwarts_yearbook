4845ce3d01e07bde637baaf2c128e2cf12034cc4
Thomas Gregory <thomjgregory@gmail.com>
2019-06-10 10:14:40 -0500

    GraphQL connected to the Harry Potter API, information is flowing

--- schema.js
+++ schema.js
@@ -1,10 +1,38 @@
 const axios = require('axios');
-const { GraphQLObjectType, GraphQLBoolean, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql')
+const config = require('config');
 
-const Character = new GraphQLObjectType({
-  name: 'Character',
-  fields: () => ({
-    name: { type: GraphQLString},
-    house: { type: GraphQLString }
-  })
-})
+const {
+	GraphQLObjectType,
+	GraphQLBoolean,
+	GraphQLString,
+	GraphQLSchema,
+	GraphQLList
+} = require('graphql');
+
+const CharacterType = new GraphQLObjectType({
+	name: 'Character',
+	fields: () => ({
+		name: { type: GraphQLString },
+		house: { type: GraphQLString }
+	})
+});
+
+const params = config.get('access');
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: () => ({
+		characters: {
+			type: new GraphQLList(CharacterType),
+			resolve(parent, args) {
+				return axios
+					.get('https://www.potterapi.com/v1/characters', params)
+					.then(res => res.data);
+			}
+		}
+	})
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
+});
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema.js');
+const cors = require('cors');
+const path = require('path');
 
 // Initailize express, parse JSON
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true
+	})
+);
 
 // Set the port, set a server on the port...
 const port = process.env.PORT || 5000;
