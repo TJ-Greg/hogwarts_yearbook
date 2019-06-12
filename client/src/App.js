3fef3af4d126a532186228cec371f92502fb6a44
Thomas Gregory <thomjgregory@gmail.com>
2019-06-11 20:31:13 -0500

    GraphQL and Apollo are working on the front end, data is showing in the console

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'reactstrap';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 
 import './App.css';
 import AppNavbar from './components/layout/AppNavbar';
 import HomePage from './components/HomePage';
 import AllCharacters from './components/AllCharacters';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const client = new ApolloClient({
+	uri: '/graphql'
+});
 
 function App() {
 	return (
 		<div className="App">
-			<Router>
-				<Fragment>
-					<AppNavbar />
-					<Container>
-						<Switch>
-							<Route exact path="/" component={HomePage} />
-							<Route exact path="/all" component={AllCharacters} />
-						</Switch>
-					</Container>
-				</Fragment>
-			</Router>
+			<ApolloProvider client={client}>
+				<Router>
+					<Fragment>
+						<AppNavbar />
+						<Container>
+							<Switch>
+								<Route exact path="/" component={HomePage} />
+								<Route exact path="/all" component={AllCharacters} />
+							</Switch>
+						</Container>
+					</Fragment>
+				</Router>
+			</ApolloProvider>
 		</div>
 	);
 }
--- client/src/components/AllCharacters.js
+++ client/src/components/AllCharacters.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const CHARACTERS_QUERY = gql`
+	query CharactersQuery {
+		characters {
+			name
+			house
+		}
+	}
+`;
 
 export default class AllCharacters extends Component {
 	render() {
@@ -7,6 +18,14 @@ export default class AllCharacters extends Component {
 			<Container className="bg-secondary border rounded text-center">
 				<div>
 					<h1>Display A list of all Characters here</h1>
+					<Query query={CHARACTERS_QUERY}>
+						{({ loading, error, data }) => {
+							if (loading) return <h4>Loading</h4>;
+							if (error) console.log(error);
+							console.log(data);
+							return <h4>Test...</h4>;
+						}}
+					</Query>
 				</div>
 			</Container>
 		);
